342c2f68245eab84469024b05518d2224764c0ef
latfur2 <latfur2@bitbucket.org>
2018-08-24 16:16:05 +0600

    Update Provider

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -16,6 +16,10 @@ class EventServiceProvider extends ServiceProvider{
        $this->publishes([
         __DIR__.'/assets' => public_path('vendor/event'),], 'public');
        
+     $this->publishes([
+        __DIR__.'/views' => resource_path('views/vendor/event'),
+    ]);
+       
     }
      public function register(){
         
